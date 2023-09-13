7c2d979b4b191d035c607ac1cc553be49f8ee91d
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2023-09-13 21:27:06 +0200

    Lesson 03: var (string, int, float)

---
+++ main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+func main() {
+	// var nameOne string = "mario"
+	// var nameTwo = "luigi"
+	// var nameThree string
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameOne = "peach"
+	// nameThree = "bowser"
+
+	// fmt.Println(nameOne, nameTwo, nameThree)
+
+	// nameFour := "yoshi"
+	// fmt.Println(nameFour)
+
+	var ageOne int = 20
+	var ageTwo = 30
+	ageThree := 40
+
+	fmt.Println(ageOne, ageTwo, ageThree)
+
+	// var numOne int8 = 25
+	// var numTwo int8 = -128
+	// var numThree uint8 = 255
+
+	var scoreOne float32 = 25.98
+	var scoreTwo float64 = 356789.12345
+	scoreThree := 1.5
+}
