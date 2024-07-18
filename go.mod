b7daa6105151aa1789ab88b9012e428c5a2ea6a7
Stelzi79 <969932+Stelzi79@users.noreply.github.com>
2024-07-18 18:46:34 +0200

    some testing

--- go.mod
+++ go.mod
@@ -1,3 +1,20 @@
 module main
 
 go 1.22.2
+
+require github.com/urfave/cli/v2 v2.27.2
+
+require (
+	github.com/mattn/go-colorable v0.1.13 // indirect
+	github.com/mattn/go-isatty v0.0.20 // indirect
+	golang.org/x/sys v0.18.0 // indirect
+)
+
+require (
+	github.com/BurntSushi/toml v1.3.2 // indirect
+	github.com/cpuguy83/go-md2man/v2 v2.0.4 // indirect
+	github.com/fatih/color v1.17.0
+	github.com/russross/blackfriday/v2 v2.1.0 // indirect
+	github.com/xrash/smetrics v0.0.0-20240312152122-5f08fbb34913 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
+)
--- main.go
+++ main.go
@@ -1,31 +1,53 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/fatih/color"
+	"github.com/urfave/cli/v2"
+	"github.com/urfave/cli/v2/altsrc"
+)
 
 func main() {
-	// x := 0
-	// for x < 5 {
-	// 	fmt.Println("Value of x is:", x)
-	// 	x++
-	// }
-
-	// for i := 0; i < 5; i++ {
-	// 	fmt.Println("Value of i is:", i)
-	// }
-
-	names := []string{"John", "Paul", "George", "Ringo"}
-
-	// for i := 0; i < len(names); i++ {
-	// 	fmt.Println(names[i])
-	// }
-
-	for index, value := range names {
-		fmt.Printf("The index is %v and the value is %v\n", index, value)
+	flags := []cli.Flag{
+		altsrc.NewIntFlag(&cli.IntFlag{Name: "test"}),
+		&cli.StringFlag{Name: "load"},
 	}
 
-	for _, value := range names {
-		fmt.Printf("The value is %v\n", value)
+	app := &cli.App{
+		Action: func(*cli.Context) error {
+			fmt.Println("--test value.*default: 0")
+			return nil
+		},
+		Before: altsrc.InitInputSourceWithContext(flags, altsrc.NewYamlSourceFromFlagFunc("load")),
+		Flags:  flags,
 	}
+	// var Reset = "\033[0m"
+	// var Red = "\033[31m"
+	// var Green = "\033[32m"
+	// var Yellow = "\033[33m"
+	// var Blue = "\033[34m"
+	// var Magenta = "\033[35m"
+	// var Cyan = "\033[36m"
+	// var Gray = "\033[37m"
+	// var White = "\033[97m"
+	// println(Red + "This is 🚧 Red" + Reset)
+	// println(Green + "This is Green" + Reset)
+	// println(Yellow + "This is Yellow" + Reset)
+	// println(Blue + "This is Blue" + Reset)
+	// println(Magenta + "This is Purple" + Reset)
+	// println(Cyan + "This is Cyan" + Reset)
+	// println(Gray + "This is Gray" + Reset)
+	// println(White + "This is White" + Reset)
 
-	fmt.Println(names)
+	println(color.RedString("This is Red"))
+	println(color.GreenString("This is Green"))
+	println(color.YellowString("This is Yellow"))
+	println(color.BlueString("This is Blue"))
+	println(color.MagentaString("This is Purple"))
+	println(color.CyanString("This is Cyan"))
+
+
+	app.Run(os.Args)
 }
